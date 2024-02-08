19b5630ff154afeaeab55fd0be90ea065cb9c6d3
Vinicius Tavares <vinicius.tavares@q2pay.com.br>
2024-02-08 13:28:04 -0300

    add intermediate

--- README.md
+++ README.md
@@ -1,15 +1,4 @@
-Welcome to your new dbt project!
+Welcome to my case !
 
-### Using the starter project
+### What you will see here
 
-Try running the following commands:
-- dbt run
-- dbt test
-
-
-### Resources:
-- Learn more about dbt [in the docs](https://docs.getdbt.com/docs/introduction)
-- Check out [Discourse](https://discourse.getdbt.com/) for commonly asked questions and answers
-- Join the [chat](https://community.getdbt.com/) on Slack for live discussions and support
-- Find [dbt events](https://events.getdbt.com) near you
-- Check out [the blog](https://blog.getdbt.com/) for the latest news on dbt's development and best practices
---
+++ models/intermediate/payment/int_payment__reports.sql
@@ -0,0 +1,32 @@
+{{
+    config(
+      materialized = 'incremental',
+      unique_key = 'acceptance_ref',
+      dist = 'acceptance_date_time',
+      incremental_strategy = 'delete+insert'
+    )
+}}
+
+
+SELECT
+    a.acceptance_ref,
+    a.acceptance_status,
+    a.acceptance_source,
+    a.acceptance_internal_ref,
+    a.acceptance_date_time,
+    a.acceptance_state,
+    a.acceptance_cvv_provided,
+    a.acceptance_amount,
+    a.acceptance_country,
+    a.acceptance_currency,
+    a.acceptance_rates,
+    b.chargeback_status,
+    b.chargeback_source,
+    b.chargeback_flag
+FROM {{ ref('stg_globepay__acceptance_reports') }} a
+LEFT JOIN {{ ref('stg_globepay__chargeback_reports') }} b
+ON a.acceptance_ref = b.chargeback_ref
+
+{% if is_incremental() %}
+    and acceptance_date_time >= (select max(acceptance_date_time) from {{ this }})
+{% endif %}
